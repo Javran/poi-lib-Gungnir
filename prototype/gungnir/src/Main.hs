7757e69c335d83429e782d192a536c51507bbdb6
Javran Cheng <Javran.c@gmail.com>
2018-03-11 22:45:00 -0400

    parsing kc3 data

--- prototype/gungnir/src/Main.hs
+++ prototype/gungnir/src/Main.hs
@@ -2,6 +2,10 @@ module Main where
 
 import System.Environment
 import System.Exit
+-- import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Lazy.Char8 as BSLC
+import Data.Aeson
+import Data.Either
 
 {-
 
@@ -13,7 +17,17 @@ TODO: battle data loader
 -}
 
 kc3Loader :: [String] -> IO ()
-kc3Loader _ = pure ()
+kc3Loader args = case args of
+    fname:_ -> do
+        raws <- BSLC.lines <$> BSLC.readFile fname
+        let decodedRaws = eitherDecode <$> raws :: [Either String Value]
+            sCount = length (rights decodedRaws) :: Int
+            eCount = length (lefts decodedRaws) :: Int
+        putStrLn $ "(success, error) = " ++ show (sCount, eCount)
+        pure ()
+    _ -> do
+        putStrLn "gungnir kc3 <filename>"
+        exitFailure
 
 poiLoader :: [String] -> IO ()
 poiLoader _ = pure ()
