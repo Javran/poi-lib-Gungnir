b27063c3ca2fa4d2accc5be461b15e678dde5186
Javran Cheng <Javran.c@gmail.com>
2018-03-11 23:05:10 -0400

    filter battle data

--- prototype/gungnir/src/Main.hs
+++ prototype/gungnir/src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import System.Environment
@@ -5,7 +6,9 @@ import System.Exit
 -- import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Lazy.Char8 as BSLC
 import Data.Aeson
+import Data.Aeson.Types
 import Data.Either
+import qualified Data.Text as T
 
 {-
 
@@ -16,14 +19,28 @@ TODO: battle data loader
 
 -}
 
+data KC3RawRecord = KC3RawRecord
+  { table :: T.Text
+  , record :: Object
+  } deriving Show
+
+instance FromJSON KC3RawRecord where
+    parseJSON (Object v) = KC3RawRecord <$> v .: "table" <*> v .: "record"
+    parseJSON invalid = typeMismatch "KC3RawRecord" invalid
+
 kc3Loader :: [String] -> IO ()
 kc3Loader args = case args of
     fname:_ -> do
         raws <- BSLC.lines <$> BSLC.readFile fname
-        let decodedRaws = eitherDecode <$> raws :: [Either String Value]
-            sCount = length (rights decodedRaws) :: Int
+        let decodedRaws = eitherDecode <$> raws :: [Either String KC3RawRecord]
+            parsed = rights decodedRaws
+            sCount = length parsed :: Int
             eCount = length (lefts decodedRaws) :: Int
+            battleRaws = filter isBattle parsed
+              where
+                isBattle d = table d == "battle"
         putStrLn $ "(success, error) = " ++ show (sCount, eCount)
+        putStrLn $ "battle count = " ++ show (length battleRaws)
         pure ()
     _ -> do
         putStrLn "gungnir kc3 <filename>"
