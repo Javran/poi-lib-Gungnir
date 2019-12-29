0a97cc521305549c9781a72fd0169a2a51ee1844
Javran Cheng <Javran.C@gmail.com>
2019-12-28 23:12:03 -0800

    a bit more typing for the sample at hand.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import { Battle } from './kcsapi'
+
 export interface BattleDetailData {
   version: string
   desc?: string
   packet: Array<Packet>
 }
 
-export type Packet = object
+export type Packet = Array<Battle>
 
 export const fromPoiBattleDetail = (raw: BattleDetailData): Array<Packet> => raw.packet
--- src/kcsapi.ts
+++ src/kcsapi.ts
@@ -1,19 +1,33 @@
 /*
   kcsapi is intended to closely match the structure of game's current API.
+
+  Reference:
+  https://github.com/andanteyk/ElectronicObserver/blob/master/ElectronicObserver/Other/Information/apilist.txt
+
  */
 
+// 1=単縦陣, 2=複縦陣, 3=輪形陣, 4=梯形陣, 5=単横陣, 6=警戒陣
+// "11"-"14"=第n警戒航行序列
 type Formation = number | string
+
+// 1=同航戦, 2=反航戦, 3=T字有利, 4=T字不利
 type Engagement = number
+
+// 1=成功, 2=失敗?, 5=失敗
 type Detection = number
+
 type IntFlag = 0 | 1
 type Contact = number
 type Airpower = number
-type SupportFlag = number
 type ShipIndex = number
 type AttackType = number
 // Damage with escort flag
+// escort flag is present when there is a ".1" bit
+// e.g. 10.1 instead of just 10.
 type DamageE = number
 type Damage = number
+
+// 0=miss, 1=hit, 2=critical
 type CriticalFlag = number
 
 interface KoukuPlaneInfo {
@@ -25,6 +39,7 @@ interface KoukuPlaneInfo {
 
 interface KoukuStage1 extends KoukuPlaneInfo {
   api_disp_seiku?: Airpower
+  api_touch_plane: any // TODO
 }
 
 type KoukuStage2 = KoukuPlaneInfo
@@ -45,9 +60,13 @@ interface KoukuStage3Friend {
 
 type KoukuStage3 = KoukuStage3Friend & KoukuStage3Enemy
 
+// there are in total 3 stages per air battle,
+// this flag is used as an indicate whether a particular stage exists
+// (a non-existing one has that corresponding stage set to null)
 type KoukuStageFlags = [IntFlag, IntFlag, IntFlag]
 
 interface Kouku {
+  api_plane_from: any // TODO?
   api_stage1: KoukuStage1 | null
   api_stage2: KoukuStage2 | null
   api_stage3: KoukuStage3 | null
@@ -61,6 +80,7 @@ interface KoukuLbas extends Kouku {
 }
 
 interface Hougeki {
+  // Note: all those top-level arrays are of the same length.
   api_at_eflag: Array<IntFlag>
   api_at_list: Array<ShipIndex>
   api_at_type: Array<AttackType>
@@ -88,47 +108,71 @@ interface RaigekiEnemy {
 
 type Raigeki = RaigekiFriend & RaigekiEnemy
 
+// [<火力>, <雷装>, <対空>, <装甲>]
 type ShipParam = [number, number, number, number]
 
-interface Battle {
+export interface Battle {
+
   api_deck_id: number
   api_formation: [Formation, Formation, Engagement]
+
+  // "_f_" for friendly units (ourselves)
   api_f_nowhps: Array<number>
   api_f_maxhps: Array<number>
+
   api_fParam: Array<ShipParam>
+
+  // enemy ship ids.
   api_ship_ke: Array<number>
+  // enemy ship levels.
   api_ship_lv: Array<number>
+
   api_ship_ke_combined?: Array<number>
   api_ship_lv_combined?: Array<number>
+
   api_e_nowhps: Array<number>
   api_e_maxhps: Array<number>
+
+  // those two only exists if the enemy fleet is combined.
   api_e_nowhps_combined?: Array<number>
   api_e_maxhps_combined?: Array<number>
+
   api_eSlot: Array<Array<number>>
   api_eParam: Array<ShipParam>
+
   api_midnight_flag: IntFlag
-  api_escape_idx: any
-  api_escape_idx_combined: any
-  api_combat_ration: any
-  api_combat_ration_combined: any
   api_search: [Detection, Detection]
-  // TODO
-  api_air_base_injection?: any
-  api_injection_kouku?: Kouku
-  api_air_base_attack: Array<KoukuLbas>
   api_stage_flag: KoukuStageFlags
   api_kouku: Kouku
-  api_support_flag: SupportFlag
-  api_support_info: any | null
-  api_stage_flag2?: SupportFlag
-  api_kouku2?: Kouku
+
+  api_support_flag: IntFlag
+  api_support_info: any | null // TODO
   api_opening_taisen_flag: IntFlag
-  api_opening_taisen: any | null
+  api_opening_taisen: any | null // TODO
   api_opening_flag: IntFlag
   api_opening_attack: Raigeki | null
+
   api_hourai_flag: [IntFlag, IntFlag, IntFlag, IntFlag]
+  // TODO:
+  // the following 4 fields are kind of context-sensitive to battle types:
+  // STF and CTF has a different ordering of those
+  // which we can guess from how these fields are ordered in the original packet.
+  // I guess for the data structure we'll be using, an Array will be appropriate.
   api_hougeki1: Hougeki | null
   api_hougeki2: Hougeki | null
   api_hougeki3: Hougeki | null
   api_raigeki: Raigeki | null
+
+  // TODO
+  api_escape_idx?: any
+  api_escape_idx_combined?: any
+  api_combat_ration: any
+  api_combat_ration_combined: any
+
+  api_air_base_injection?: any
+  api_injection_kouku?: Kouku
+  api_air_base_attack: Array<KoukuLbas>
+
+  api_stage_flag2?: KoukuStageFlags
+  api_kouku2?: Kouku
 }
