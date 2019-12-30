c6e728207cf12bd86a9f5bb9ee0751bfb1d3947d
Javran Cheng <Javran.C@gmail.com>
2019-12-30 00:08:44 -0800

    a bit more stuff.

--- src/yapi.ts
+++ src/yapi.ts
@@ -3,14 +3,40 @@
   (in case you wonder, Y is for Yggdrasil)
  */
 
+/*
+  For information that can be organized into "friend" side
+  and "enemy" side.
+ */
+export interface TwoSides<T> {
+  friend: T,
+  enemy: T,
+}
+
 export enum Engagement {
   Parallel = 1,
-  Headon = 2,
+  HeadOn = 2,
   TAdvantage = 3,
   TDisadvantage = 4,
 }
 
+export enum Formation {
+  LineAhead = 1,
+  DoubleLine = 2,
+  Diamond = 3,
+  Echelon = 4,
+  LineAbreast = 5,
+  Vanguard = 6,
+  CruisingFormation1 = 11,
+  CruisingFormation2 = 12,
+  CruisingFormation3 = 13,
+  CruisingFormation4 = 14,
+}
+
+export type HP = [number, number] // current and max.
+
 export interface Battle {
   deck_id: number
   engagement: Engagement
+  formation: TwoSides<Formation>
+  hps: TwoSides<Array<HP>>
 }
