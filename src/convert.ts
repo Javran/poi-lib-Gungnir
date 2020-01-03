92459307f23e6ec2f21d33aebb3e9f885744f9e8
Javran Cheng <Javran.C@gmail.com>
2020-01-03 00:02:40 -0800

    convert engagement and formation.

--- src/convert.ts
+++ src/convert.ts
@@ -8,12 +8,25 @@ export const convertEngagement = (raw: kcsapi.Engagement): yapi.Engagement => {
   throw new Error(`Cannot convert Engagement ${raw}.`)
 }
 
+export const convertFormation = (raw: kcsapi.Formation): yapi.Formation => {
+  if (raw >= 1 && raw <= 6) {
+    return raw as yapi.Formation
+  }
+  switch (raw) {
+    case '11': return yapi.Formation.CruisingFormation1
+    case '12': return yapi.Formation.CruisingFormation2
+    case '13': return yapi.Formation.CruisingFormation3
+    case '14': return yapi.Formation.CruisingFormation4
+    default: throw new Error(`Cannot convert Formation ${raw}.`)
+  }
+}
+
 export const convertBattle = (raw: kcsapi.Battle): yapi.Battle => {
   const [fForm, eForm, engagement] = raw.api_formation
   return {
     deck_id: raw.api_deck_id,
     engagement: convertEngagement(engagement),
-    formation: { friend: yapi.Formation.LineAhead, enemy: yapi.Formation.LineAhead }, // TODO
+    formation: { friend: convertFormation(fForm), enemy: convertFormation(eForm) },
     hps: { friend: [], enemy: [] }, // TODO
   }
 }
