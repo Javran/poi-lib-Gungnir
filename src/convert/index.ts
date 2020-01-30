3a8cc0a8e4bccaf4af4d3123d2133532c8c56995
Javran Cheng <Javran.C@gmail.com>
2020-01-29 22:16:36 -0800

    a bit refactoring.

--- src/convert/index.ts
+++ src/convert/index.ts
@@ -182,59 +182,54 @@ export const convertSupportType = (flag: number): yapi.SupportType | null =>
       : new yapi.Unknown(flag, 'SupportType')
 
 // TODO: test.
-export const convertSupportInfo = (flag: number, raw: kcsapi.SupportInfo): yapi.SupportInfo | null => {
-  const type = convertSupportType(flag)
-  if (type === yapi.SupportTypeE.Shelling || type === yapi.SupportTypeE.Torpedo) {
-    // should convert to SupportInfoHourai
-    const rawDetail = raw.api_support_hourai
-    if (rawDetail === null) {
-      throw new Error(`api_support_hourai shouldn't be null.`)
+export const convertSupportInfo =
+  (flag: number, raw: kcsapi.SupportInfo): yapi.SupportInfo | null => {
+    const type = convertSupportType(flag)
+    const convertShips =
+      (
+        shipIds: Array<number>,
+        undressingFlags: Array<kcsapi.IntFlag>
+      ): Array<yapi.SupportInfoShip> => (_.zipWith as any)(
+        shipIds, undressingFlags,
+        (rosterId: number, uFlg: kcsapi.IntFlag) => ({
+          rosterId,
+          undressing: convertIntFlag(uFlg),
+        }))
+    if (type === yapi.SupportTypeE.Shelling || type === yapi.SupportTypeE.Torpedo) {
+      // should convert to SupportInfoHourai
+      const rawDetail = raw.api_support_hourai
+      if (rawDetail === null) {
+        throw new Error(`api_support_hourai shouldn't be null.`)
+      }
+      const attackInfo: Array<yapi.SupportInfoHouraiDamage> = (_.zipWith as any)(
+        rawDetail.api_cl_list,
+        rawDetail.api_damage,
+        (cl: kcsapi.CriticalFlag, dmg: kcsapi.DamageE) => ({
+          critical: convertCritical(cl),
+          damage: convertDamageWithFlag(dmg),
+        })
+      )
+      return {
+        type,
+        deckId: rawDetail.api_deck_id,
+        ships: convertShips(rawDetail.api_ship_id, rawDetail.api_undressing_flag),
+        attackInfo,
+      }
     }
-    const ships: Array<yapi.SupportInfoShip> = (_.zipWith as any)(
-      rawDetail.api_ship_id,
-      rawDetail.api_undressing_flag,
-      (rosterId: number, uFlg: kcsapi.IntFlag) => ({
-        rosterId,
-        undressing: convertIntFlag(uFlg),
-      })
-    )
-    const attackInfo: Array<yapi.SupportInfoHouraiDamage> = (_.zipWith as any)(
-      rawDetail.api_cl_list,
-      rawDetail.api_damage,
-      (cl: kcsapi.CriticalFlag, dmg: kcsapi.DamageE) => ({
-        critical: convertCritical(cl),
-        damage: convertDamageWithFlag(dmg),
-      })
-    )
-    return {
-      type,
-      deckId: rawDetail.api_deck_id,
-      ships,
-      attackInfo,
+    if (type === yapi.SupportTypeE.Airstrike || type === yapi.SupportTypeE.AntiSub) {
+      const rawDetail = raw.api_support_airatack
+      if (rawDetail === null) {
+        throw new Error(`api_support_airatack shouldn't be null.`)
+      }
+      return {
+        type,
+        deckId: rawDetail.api_deck_id,
+        ships: convertShips(rawDetail.api_ship_id, rawDetail.api_undressing_flag),
+        koukuStages: convertKoukuStagesForSupport(rawDetail),
+      }
     }
+    return new yapi.Unknown({ flag, raw }, 'SupportInfo')
   }
-  if (type === yapi.SupportTypeE.Airstrike || type === yapi.SupportTypeE.AntiSub) {
-    const rawDetail = raw.api_support_airatack
-    if (rawDetail === null) {
-      throw new Error(`api_support_airatack shouldn't be null.`)
-    }
-    const ships: Array<yapi.SupportInfoShip> = (_.zipWith as any)(
-      rawDetail.api_ship_id,
-      rawDetail.api_undressing_flag,
-      (rosterId: number, uFlg: kcsapi.IntFlag) => ({
-        rosterId,
-        undressing: convertIntFlag(uFlg),
-      })
-    )
-    return {
-      type,
-      deckId: rawDetail.api_deck_id,
-      ships,
-      koukuStages: convertKoukuStagesForSupport(rawDetail),
-    }
-  }
-  return new yapi.Unknown({ flag, raw }, 'SupportInfo')
-}
 
 export const convertBattle = (raw: kcsapi.Battle): yapi.Battle => {
   // IIFE for now, until do-expression becomes available.
